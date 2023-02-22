c2b33fa6cfc54cddd8f1e9323a56537723cb4f30
CarlosGozalbes <carlosfgmendoza@gmail.com>
2023-02-22 14:41:18 +0100

    finish ready to go

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import React, { lazy,Suspense } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import * as ROUTES from './constants/routes'
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import ReactLoader from "./components/Loader";
+import * as ROUTES from "./constants/routes";
+import UserContext from "./context/user";
+//import ProtectedRoute from "./helpers/protected-route";
+import useAuthListener from "./hooks/use-auth-listener";
 
-const Login = lazy(()=> import ("./pages/Login"))
+const Login = lazy(() => import("./pages/Login"));
+const SignUp = lazy(() => import("./pages/Signup"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Profile = lazy(() => import("./pages/Profile"));
+const NotFound = lazy(() => import("./pages/NotFound"));
 
-function App() {
+export default function App() {
+  const { user } = useAuthListener();
+  function ProtectedRoute({ user, children }) {
+   return user ? children : <Navigate to={ROUTES.LOGIN} />;
+  }
   return (
-    <Router>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Routes>
-          
-          <Route path={ROUTES.LOGIN} element={<Login />} />
-        </Routes>
-      </Suspense>
-    </Router>
+    <UserContext.Provider value={{ user }}>
+      <Router>
+        <Suspense fallback={<ReactLoader />}>
+          <Routes>
+            <Route path={ROUTES.LOGIN} element={<Login />} />
+            <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
+            <Route path={ROUTES.PROFILE} element={<Profile />} />
+            <Route
+              path={ROUTES.DASHBOARD}
+              element={
+                <ProtectedRoute user={user}>
+                  <Dashboard user={user} />
+                </ProtectedRoute>
+              }
+            />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </UserContext.Provider>
   );
 }
-
-export default App;
